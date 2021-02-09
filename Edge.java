e9de3664eabe26745713354bb8eab6eec9c605c1
tmfpdata <78817225+tmfpdata@users.noreply.github.com>
2021-02-09 19:21:02 +0100

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,39 @@
+public class Edge{
+	
+	Edge(Node source, Node target)
+	
+	
+	public Node source(){
+
+	}
+
+	
+	public Node target (){
+		
+		
+	}
+	
+	
+	public int pheromones (){
+		
+		
+	}
+	
+	
+	public void raisePheromones(int amount){
+		
+		
+		
+	}
+	
+	
+	public void decreasePheromones(){
+		
+		
+		
+		
+	}
+	
+	
+	
+}
