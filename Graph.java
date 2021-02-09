8b0c0b1d31076f1d5661a7155da5128635c23091
hehoe20 <73895114+hehoe20@users.noreply.github.com>
2021-02-09 13:57:20 +0100

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,21 @@
+public class Graph {
+	
+	public Graph(int width, int depth, Colony[] homes, double sugarProbability, int avgSugar) {
+	}
+
+	public Graph(String filename, Colony[] homes, double sugarProbability, int avgSugar) {
+	}
+	
+	public Node[] adjacentTo(Node node) {
+	}
+
+	public void tick() {
+	}
+	
+	public void raisePheromones(Node source, Node target, int amount) {
+	}
+
+	public void raisePheromones(Node source, Node target, int amount) {
+	}
+	
+}
