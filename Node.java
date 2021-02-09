3677ef5852c4ff954c8235d94cdae78d56354764
tmfpdata <78817225+tmfpdata@users.noreply.github.com>
2021-02-09 19:31:04 +0100

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,24 @@
+public class Node{
+	
+	public Node ()
+	
+	public Node (int sugar)
+	
+
+	public int sugar (){
+		
+		
+	}
+	
+	public void decreaseSugar(){
+		
+		
+		
+	}
+	
+	public void setSugar(int sugar){
+		
+		
+	}
+	
+}
